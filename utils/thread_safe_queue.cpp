f1c5a7ff38381b59ddc8cad984509fa4de2e06be
Leonard1 <11381195+leonard1@user.noreply.gitee.com>
2023-05-16 15:20:17 +0800

    modify thread safe queue.

--- utils/thread_safe_queue.cpp
+++ utils/thread_safe_queue.cpp
@@ -5,13 +5,22 @@ template <typename T>
 thread_safe_queue<T>::thread_safe_queue()
 {}
 
+// copy, copy assign, move, move assign 
 template <typename T>
-thread_safe_queue<T>::thread_safe_queue(thread_safe_queue const & other)
+thread_safe_queue<T>::thread_safe_queue(thread_safe_queue<T> const & other)
 {
     std::lock_guard<std::mutex> lock(other.mtx);
-    conn_queue = other.conn_queue;
+    this->conn_queue = other.conn_queue;
 }
 
+template <typename T>
+thread_safe_queue<T> & thread_safe_queue<T>::operator = (const thread_safe_queue<T> &other)
+{
+    std::lock_guard<std::mutex> lock(other.mtx);
+    this->conn_queue = other.conn_queue;
+}
+
+// push 
 template <typename T>
 void thread_safe_queue<T>::push (T value)
 {
--- utils/thread_safe_queue.h
+++ utils/thread_safe_queue.h
@@ -17,8 +17,13 @@ class thread_safe_queue
         
     public:
         // constructor 
-        thread_safe_queue();
-        thread_safe_queue(thread_safe_queue const & other);
+        explicit thread_safe_queue();
+
+        // copy, copy assign, move, move assign 
+        thread_safe_queue (const thread_safe_queue<T> &other);
+        thread_safe_queue<T> & operator = (const thread_safe_queue<T> &other);
+        thread_safe_queue (const thread_safe_queue<T> && other) = delete;
+        thread_safe_queue<T> & operator = (const thread_safe_queue<T> && other) = delete;
 
         void push(T value);
         bool empty();
